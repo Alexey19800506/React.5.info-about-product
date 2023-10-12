6d35b67722a4216d8956b9d9dc85925c3ce5f342
Alexey19800506 <osiyuk19800506@gmail.com>
2023-10-12 12:41:29 +0300

    tas3

--- src/Page.jsx
+++ src/Page.jsx
@@ -20,7 +20,7 @@ class Page extends React.Component {
       <div className="page">
         <Info text={this.state.text}/>
         <div className="actions">
-          <button className="btn" onClick={() => this.setText('IPhone 13 - Price is 500$. Available in 2 colors')}>IPhone 13</button>
+          <button className="btn" onClick={() => this.setText('Price is 500$. Available in 2 colors')}>IPhone 13</button>
           <button className="btn" onClick={() => this.setText('Price is 650$. Not available')}>IPhone 13 Pro</button>
           <button className="btn" onClick={() => this.setText('')}>Clear</button>
         </div>
