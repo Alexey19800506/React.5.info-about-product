0b68809ecbb0e9540db2108d34647fd9124f0036
Alexey19800506 <osiyuk19800506@gmail.com>
2023-10-12 12:40:05 +0300

    t3

---
+++ src/Info.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Info = ({text}) => {
+    if (!text) {
+        return null;
+    }
+    return (
+        <div className='info'>
+           {text}
+        </div>
+    );
+};
+
+export default Info;
---
+++ src/Page.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Info from './Info';
+
+class Page extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: null
+    };  
+  }
+
+  setText = text => {
+      this.setState({
+        text
+      })
+  };
+
+  render(){
+    return (
+      <div className="page">
+        <Info text={this.state.text}/>
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText('IPhone 13 - Price is 500$. Available in 2 colors')}>IPhone 13</button>
+          <button className="btn" onClick={() => this.setText('Price is 650$. Not available')}>IPhone 13 Pro</button>
+          <button className="btn" onClick={() => this.setText('')}>Clear</button>
+        </div>
+      </div>
+      
+    );
+
+  }
+}
+       
+export default Page;
+
